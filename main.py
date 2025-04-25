6406c5e35fdd93856aeb2bb948843d4733c67240
PiGuTan <pigutheboss@gmail.com>
2025-04-25 18:08:43 +0800

    added links to discord

---
+++ main.py
@@ -0,0 +1,55 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+intents.message_content = True
+
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"We are ready to go in, {bot.user.name}")
+
+@bot.event
+async def on_message(message):
+    if message.author.bot or not message.content.startswith(f'<@{bot.user.id}>'):
+        return
+    content = message.content.split(" ",maxsplit=1)[-1]
+    logging.debug(f"Received message: {content}")
+
+    await message.channel.send(content)
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
+
+@bot.command()
+async def dm(ctx, *, msg):
+    await ctx.author.send(f"You said {msg}")
+
+@bot.command()
+async def reply(ctx):
+    await ctx.reply("This is a reply to your message!")
+
+@bot.command()
+async def poll(ctx, *, question):
+    embed = discord.Embed(title="New Poll", description=question)
+    poll_message = await ctx.send(embed=embed)
+    await poll_message.add_reaction("👍")
+    await poll_message.add_reaction("👎")
+
+
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
