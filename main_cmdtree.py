e8408b5d147b864ab9404885a19e43a1f0f17c1e
PiGuTan <pigutheboss@gmail.com>
2026-03-08 20:47:47 +0800

    migrate to cmd tree

--- main_cmdtree.py
+++ main_cmdtree.py
@@ -40,5 +40,28 @@ async def on_ready():
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f"Hello {interaction.user.mention}!")
 
+@client.tree.command(name="copy", description="imitates a users character with an action and expression")
+async def copy(interaction: discord.Interaction, ign:str,action:str=None,expression:str=None):
+    logging.debug(f"command_receive|ign={ign}, action={action}, expression={expression}")
+    await interaction.response.defer(thinking=True)
 
-client.run(token)
+    image_url = Character(ign).image_url
+    if not image_url:
+        await interaction.followup.send(f"Who is {ign}:question:")
+        return
+
+    if not (action or expression):
+        await interaction.followup.send(content=image_url)
+        return
+    try:
+        content = [i for i in (action,expression) if i is not None]
+        param, a_frames, e_frames = content_processorr.process_split_string(content)
+        image = Character_Image(image_url,params=param)
+        image.get_images(a_frames=a_frames, e_frames=e_frames) # a_frame, e_frame passed as none
+        output_bytes = image.process_image()
+        file_name = content_processorr.parse_file_name(content,param)
+        await interaction.followup.send(file=discord.File(io.BytesIO(output_bytes), filename=file_name))
+    except Exception as e:
+        await interaction.followup.send(f"imposter shot curcuited with error{e}")
+
+client.run(token, log_handler=handler, log_level=logging.DEBUG)
