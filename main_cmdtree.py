461fb74c5886f507c3bd309842802cc5bd328535
PiGuTan <pigutheboss@gmail.com>
2026-03-08 22:46:24 +0800

    added logger

--- main_cmdtree.py
+++ main_cmdtree.py
@@ -16,7 +16,15 @@ from core import Character
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
+# logging
+bot_logger = logging.getLogger('bot_actions')
+bot_logger.setLevel(logging.INFO)
+bot_handler = logging.FileHandler(filename='actions.log', encoding='utf-8', mode='w')
+bot_formatter = logging.Formatter('%(asctime)s|%(interaction_id)s|%(command)s|%(result)s|%(message)s')
+bot_logger.addHandler(bot_handler)
+bot_handler.setFormatter(bot_formatter)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
@@ -42,15 +50,25 @@ async def hello(interaction: discord.Interaction):
 
 @client.tree.command(name="copy", description="imitates a users character with an action and expression")
 async def copy(interaction: discord.Interaction, ign:str,action:str=None,expression:str=None):
-    logging.debug(f"command_receive|ign={ign}, action={action}, expression={expression}")
+    log_extra = {
+        "interaction_id": interaction.id,
+        "command": "copy",
+        "result": "-"
+    }
+    bot_logger.info(f"ign={ign}, action={action}, expression={expression}",
+                    extra={**log_extra,"result":"receive"})
     await interaction.response.defer(thinking=True)
 
     image_url = Character(ign).image_url
     if not image_url:
+        bot_logger.info(f"ign not found ign={ign}",
+                        extra={**log_extra, "result": "error"})
         await interaction.followup.send(f"Who is {ign}:question:")
         return
 
     if not (action or expression):
+        bot_logger.info(f"no action or expression found",
+                        extra={**log_extra, "result": "success"})
         await interaction.followup.send(content=image_url)
         return
     try:
@@ -60,8 +78,12 @@ async def copy(interaction: discord.Interaction, ign:str,action:str=None,express
         image.get_images(a_frames=a_frames, e_frames=e_frames) # a_frame, e_frame passed as none
         output_bytes = image.process_image()
         file_name = content_processorr.parse_file_name(content,param)
+        bot_logger.info("",
+                        extra={**log_extra, "result": "success"})
         await interaction.followup.send(file=discord.File(io.BytesIO(output_bytes), filename=file_name))
     except Exception as e:
-        await interaction.followup.send(f"imposter shot curcuited with error{e}")
+        bot_logger.error(f"error={e}",
+                        extra={**log_extra, "result": "error"})
+        await interaction.followup.send(f"imposter shot circuited with error\n{e}")
 
 client.run(token, log_handler=handler, log_level=logging.DEBUG)
