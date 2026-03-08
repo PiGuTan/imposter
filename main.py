6ae465181f8f1ba9a21526e672c592984d9e1b79
PiGuTan <pigutheboss@gmail.com>
2026-03-08 23:06:16 +0800

    replaced main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import logging
 from dotenv import load_dotenv
 import os
 import io
-import asyncio
+
+import ctypes
+from discord.ext import commands
+from dotenv import load_dotenv
 
 from core import Character_Image
 from core import content_processorr
@@ -13,53 +16,77 @@ from core import Character
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
+# logging
+bot_logger = logging.getLogger('bot_actions')
+bot_logger.setLevel(logging.INFO)
+bot_handler = logging.FileHandler(filename='actions.log', encoding='utf-8', mode='w')
+bot_formatter = logging.Formatter('%(asctime)s|%(interaction_id)s|%(command)s|%(result)s|%(message)s')
+bot_logger.addHandler(bot_handler)
+bot_handler.setFormatter(bot_formatter)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
 intents.message_content = True
 
+client = commands.Bot(command_prefix="!", intents=intents)
+client.remove_command("help")
 
-bot = commands.Bot(command_prefix='!', intents=intents)
-
-@bot.event
+@client.event
 async def on_ready():
-    print(f"We are ready to go in, {bot.user.name}")
+    try:
+        synced = await client.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
 
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        await bot.process_commands(message)
+    activity = discord.Activity(type=discord.ActivityType.watching, name="/hello")
+    await client.change_presence(status=discord.Status.online, activity=activity)
+
+@client.tree.command(name="hello", description="Say hello and tagging user")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hello {interaction.user.mention}!")
+
+@client.tree.command(name="copy", description="imitates a users character with an action and expression")
+@discord.app_commands.allowed_installs(guilds=True, users=True)
+@discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+async def copy(interaction: discord.Interaction, ign:str,action:str=None,expression:str=None):
+    log_extra = {
+        "interaction_id": interaction.id,
+        "command": "copy",
+        "result": "-"
+    }
+    bot_logger.info(f"ign={ign}, action={action}, expression={expression}",
+                    extra={**log_extra,"result":"receive"})
+    await interaction.response.defer(thinking=True)
+
+    image_url = Character(ign).image_url
+    if not image_url:
+        bot_logger.info(f"ign not found ign={ign}",
+                        extra={**log_extra, "result": "error"})
+        await interaction.followup.send(f"Who is {ign}:question:")
         return
-    is_guild_message : bool = bool(message.channel.guild)
-    is_ping_message : bool = message.content.startswith(f'<@{bot.user.id}>')
-    if  is_guild_message and not is_ping_message:
-        await bot.process_commands(message)
+
+    if not (action or expression):
+        bot_logger.info(f"no action or expression found",
+                        extra={**log_extra, "result": "success"})
+        await interaction.followup.send(content=image_url)
         return
+    try:
+        # TODO: change to handle two strings instead of a list of two strings
+        content = [i for i in (action,expression) if i is not None]
+        param, a_frames, e_frames = content_processorr.process_split_string(content)
+        image = Character_Image(image_url,params=param)
+        image.get_images(a_frames=a_frames, e_frames=e_frames) # a_frame, e_frame passed as none
+        output_bytes = image.process_image()
+        file_name = content_processorr.parse_file_name(content,param)
+        bot_logger.info("",
+                        extra={**log_extra, "result": "success"})
+        await interaction.followup.send(file=discord.File(io.BytesIO(output_bytes), filename=file_name))
+    except Exception as e:
+        bot_logger.error(f"error={e}",
+                        extra={**log_extra, "result": "error"})
+        await interaction.followup.send(f"imposter shot circuited with error\n{e}")
 
-    content = message.content.split(" ",maxsplit=1)[-1] if is_ping_message else message.content
-    logging.debug(f"Received message: {content}")
-
-    if is_guild_message:
-        await message.delete()
-
-    content, command = content_processorr.pre_process_content(content)
-    output = Character(content).image_url
-    if not output:
-        await message.channel.send(f"Who is {content}:question:")
-        await bot.process_commands(message)
-        return
-    if not command:
-        await message.channel.send(content=output)
-        await bot.process_commands(message)
-        return
-    param, a_frames, e_frames = content_processorr.process_split_string(command)
-    image = Character_Image(output,params=param)
-    image.get_images(a_frames=a_frames, e_frames=e_frames) # a_frame, e_frame passed as none
-    output_bytes = image.process_image()
-    file_name = content_processorr.parse_file_name(content,param)
-    await message.channel.send(file=discord.File(io.BytesIO(output_bytes), filename=file_name))
-
-    await bot.process_commands(message)
-
-bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+client.run(token, log_handler=handler, log_level=logging.DEBUG)
