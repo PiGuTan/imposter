c0d6f4c71fe2674ed02f59c10e0f2c56033a54a2
PiGuTan <103995615+PiGuTan@users.noreply.github.com>
2026-05-21 07:39:50 +0800

    Revert "mongodb integration"

--- client/__init__.py
+++ client/__init__.py
@@ -2,5 +2,3 @@ from .maplesea_data_client import Data_Agent
 from .maplesea_image_client import Image_Client
 
 from .gemini_client import Gemini_agent
-
-from .mongodb_client import DataBase_tab
--- util/exceptions.py
+++ util/exceptions.py
@@ -6,6 +6,3 @@ class MissingIGNError(Exception):
 
 class MissingAssetError(Exception):
     pass
-
-class MissingDBError(Exception):
-    pass
